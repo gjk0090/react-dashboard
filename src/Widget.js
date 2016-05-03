d6242c7a800fa526974880459975e45ebcce7216
gjk <gaojk0090@gmail.com>
2016-05-02 23:50:15 -0400

    use react bootstrap failed

--- src/Widget.js
+++ src/Widget.js
@@ -1,4 +1,5 @@
 var React = require('react');
+var Modal = require('react-bootstrap/lib/Modal');
 var WidgetList = require('./widgets');
 
 var Widget = React.createClass({
@@ -51,6 +52,14 @@ var Widget = React.createClass({
     }
   },
 
+  openConfigModal: function(){
+    this.setState({ showModal: true });
+  },
+
+  closeConfigModal: function() {
+    this.setState({ showModal: false });
+  },
+
   render: function() {
 
     var widgetStyle = {};
@@ -81,6 +90,7 @@ var Widget = React.createClass({
     }else{
       headingButtons = (
         <span className="pull-right">
+          <a title="reload widget content" style={aTagStyle} onClick={this.openConfigModal}> <i className="glyphicon glyphicon-cog"></i> </a>
           <a title="reload widget content" style={aTagStyle} onClick={this.refreshWidget}> <i className="glyphicon glyphicon-refresh"></i> </a>
         </span>
       );
@@ -103,6 +113,34 @@ var Widget = React.createClass({
               </div>
             </div>
           </div>
+
+          {/*
+          <Modal show={this.state.showModal} onHide={this.closeConfigModal}>
+            <Modal.Header closeButton>
+              <Modal.Title>Modal heading</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <h4>Text in a modal</h4>
+              <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula.</p>
+              <hr />
+
+              <h4>Overflowing text to show scroll behavior</h4>
+              <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+              <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+              <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+              <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+              <p>Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.</p>
+              <p>Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec ullamcorper nulla non metus auctor fringilla.</p>
+            </Modal.Body>
+            <Modal.Footer>
+              <button onClick={this.closeConfigModal}>Close</button>
+            </Modal.Footer>
+          </Modal>
+          */}
+
       </div>
     );
   }
